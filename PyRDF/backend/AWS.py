335a0180182d76c30d68a2cab31955bbde8a20a4
Kamilbur <kamil.burkiewicz@gmail.com>
2021-08-04 02:21:32 +0200

    ROOT files compression and PR corrections

--- PyRDF/backend/AWS.py
+++ PyRDF/backend/AWS.py
@@ -4,15 +4,19 @@ import base64
 import json
 import logging
 import time
-import threading
 import concurrent.futures
 import os
+import sys
+import ROOT
+from pathlib import Path
 
 import boto3
 import botocore
 import cloudpickle as pickle
 
 from .Dist import Dist
+
+lambda_await_thread_stop = False
 
 
 class FlushingLogger:
@@ -21,7 +25,7 @@ class FlushingLogger:
 
     def __getattr__(self, name):
         method = getattr(self.logger, name)
-        if name in ['info', ]:
+        if name in ['info', 'warning', 'debug', 'error', 'critical', ]:
             def flushed_method(msg, *args, **kwargs):
                 method(msg, *args, **kwargs)
                 for h in self.logger.handlers:
@@ -55,14 +59,11 @@ class AWS(Dist):
     def ProcessAndMerge(self, mapper, reducer):
         """
         Performs map-reduce using AWS Lambda.
-
         Args:
             mapper (function): A function that runs the computational graph
                 and returns a list of values.
-
             reducer (function): A function that merges two lists that were
                 returned by the mapper.
-
         Returns:
             list: A list representing the values of action nodes returned
             after computation (Map-Reduce).
@@ -70,12 +71,9 @@ class AWS(Dist):
 
         ranges = self.build_ranges()
 
-        def encode_object(object_to_encode) -> str:
-            return str(base64.b64encode(pickle.dumps(object_to_encode)))
-
         # Make mapper and reducer transferable
-        pickled_mapper = encode_object(mapper)
-        pickled_reducer = encode_object(reducer)
+        pickled_mapper = AWS.encode_object(mapper)
+        pickled_reducer = AWS.encode_object(reducer)
 
         # Setup AWS clients
         s3_resource = boto3.resource('s3', region_name=self.region)
@@ -89,14 +87,12 @@ class AWS(Dist):
         if not s3_output_bucket:
             self.logger.info('AWS backend not initialized!')
             return False
-
         ssm_client.put_parameter(
             Name='ranges_num',
             Type='String',
             Value=str(len(ranges)),
             Overwrite=True
         )
-
         ssm_client.put_parameter(
             Name='reducer',
             Type='String',
@@ -105,103 +101,29 @@ class AWS(Dist):
         )
         """
 
-        def invoke_root_lambda(root_range, script):
-
-            trials = 3
-
-            client = boto3.client('lambda', region_name=self.region)
-            def process_response(response):
-                if response:
-                    if 'Payload' in response:
-                        payload = response['Payload']
-                        if isinstance(payload, botocore.response.StreamingBody):
-                            payload_bytes = payload.read()
-                            try:
-                                response['Payload'] = json.loads(payload_bytes)
-                            except json.JSONDecodeError:
-                                response['Payload'] = {}
-
-                return response
-
-            payload = json.dumps({
-                'range': encode_object(root_range),
-                'script': script,
-                'start': str(root_range.start),
-                'end': str(root_range.end),
-                'filelist': str(root_range.filelist),
-                'friend_info': encode_object(root_range.friend_info)
-            })
-
-            # Maybe here give info about number of invoked lambda for awsmonitor
-            #self.logger.info(f'New lambda - 31')
-
-            while trials:
-                trials -= 1
-                try:
-                    #my_before_time = time.time()
-                    response = client.invoke(
-                        FunctionName='root_lambda',
-                        InvocationType= 'Event', #'RequestResponse',
-                        Payload=bytes(payload, encoding='utf8')
-                    )
-                    #my_after_time = time.time()
-                    #self.logger.info(f'Invocation time: {my_after_time - my_before_time}')
-
-                    if 'FunctionError' in response:
-                        raise RuntimeError('Lambda runtime error')
-
-                    response = process_response(response)
-
-                    if 'Payload' in response:
-                        response_payload = response['Payload']
-                        lambda_status = response_payload['statusCode'] if 'statusCode' in response_payload else 200
-                        if lambda_status != 200:
-                            raise RuntimeError(f'Lambda status code {lambda_status}')
-                except botocore.exceptions.ClientError as error:
-                    # AWS site errors
-                    self.logger.warn(error['Error']['Message'])
-                except RuntimeError as error:
-                    # Lambda runtime errors and lambdas that
-                    # returned statusCode different than 200
-                    # (maybe also response processing errors)
-                    self.logger.warn(error)
-                except Exception as error:
-                    # All other errors
-                    self.logger.warn(error)
-                else:
-                    break
-                time.sleep(1)
-
-            # Note: lambda finishes before s3 object is created
-            #self.logger.info('Lambda finished :)')
-
-            return response
-
-
         self.logger.info(f'Before lambdas invoke. Number of lambdas: {len(ranges)}')
 
         processing_bucket = ssm_client.get_parameter(Name='processing_bucket')['Parameter']['Value']
 
         s3_resource.Bucket(processing_bucket).objects.all().delete()
 
-
         invoke_begin = time.time()
         # Invoke workers with ranges and mapper
-        call_results = []
-        """
-        for lambda_num, root_range in enumerate(ranges):
-            call_result = invoke_root_lambda(lambda_client, root_range, pickled_mapper)
-            call_results.append(call_result)
-            self.logger.info(f'New lambda - {lambda_num}')
-        """
 
+        global lambda_await_thread_stop
+        lambda_await_thread_stop = False
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(ranges)) as executor:
-            executor.submit(AWS.process_execution, s3_client, processing_bucket, len(ranges), self.logger)
-            futures = [executor.submit(invoke_root_lambda, root_range, pickled_mapper) for root_range in ranges]
-            executor.shutdown(wait=True)
+            executor.submit(AWS.wait_for_all_lambdas, s3_client, processing_bucket, len(ranges), self.logger)
+            futures = [executor.submit(AWS.invoke_root_lambda, root_range, pickled_mapper, self.region, self.logger)
+                       for root_range in ranges]
+            call_results = [future.result() for future in futures]
+            if not all(call_results):
+                lambda_await_thread_stop = True
 
-        wait_begin = time.time()
+        if lambda_await_thread_stop:
+            raise Exception(f'Some lambdas failed after multiple retrials')
+
         self.logger.info('All lambdas have been invoked')
 
         # while True:
@@ -212,25 +134,21 @@ class AWS(Dist):
         #     time.sleep(1)
         # result = s3.get_object(s3_output_bucket, 'out.pickle')
 
+        download_begin = time.time()
+
+        # Get names of output files, download and reduce them
+        filenames = AWS.get_all_objects_from_s3_bucket(s3_client, processing_bucket)
+        self.logger.info(f'Lambdas finished: {len(filenames)}')
+
+        tmp_files_directory = '/tmp'
+        AWS.remove_all_tmp_root_files(tmp_files_directory)
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(filenames)) as executor:
+            futures = [executor.submit(AWS.get_from_s3, filename, self.region, processing_bucket, tmp_files_directory)
+                       for filename in filenames]
+            files = [future.result() for future in futures]
 
         reduce_begin = time.time()
-        # Get names of output files, download and reduce them
-        results = s3_client.list_objects_v2(Bucket=processing_bucket)
-        self.logger.info(f'Lambdas finished: {results["KeyCount"]}')
-        filenames = s3_client.list_objects_v2(Bucket=processing_bucket)['Contents']
-
-        def get_from_s3(filename):
-            s3_client = boto3.client('s3', region_name=self.region)
-            return pickle.loads(s3_client.get_object(
-                        Bucket=processing_bucket,
-                        Key=filename['Key']
-                    )['Body'].read())
-
-        files = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=len(filenames)) as executor:
-            futures = [executor.submit(get_from_s3, filename) for filename in filenames]
-            executor.shutdown(wait=True)
-            files = [future.result() for future in futures]
 
         to_process = files
         while len(to_process) > 1:
@@ -238,8 +156,8 @@ class AWS(Dist):
             odd_index_files = [to_process[i] for i in range(len(to_process)) if i % 2 == 1]
 
             with concurrent.futures.ThreadPoolExecutor(len(to_process)) as executor:
-                futures = [executor.submit(reducer, pair[0], pair[1]) for pair in zip(even_index_files, odd_index_files)]
-                executor.shutdown(wait=True)
+                futures = [executor.submit(reducer, pair[0], pair[1]) for pair in
+                           zip(even_index_files, odd_index_files)]
                 to_process = [future.result() for future in futures]
 
             if len(even_index_files) > len(odd_index_files):
@@ -254,28 +172,134 @@ class AWS(Dist):
 
         bench = (
             len(ranges),
-            wait_begin-invoke_begin,
-            reduce_begin-wait_begin,
-            time.time()-reduce_begin
+            download_begin - invoke_begin,
+            reduce_begin - download_begin,
+            time.time() - reduce_begin
         )
 
         print(bench)
 
         return reduction_result
-        # reduced_output = pickle.loads(result)
-        # return reduced_output
 
     def distribute_files(self, includes_list):
         pass
 
     @staticmethod
-    def process_execution(s3_client, processing_bucket, num_of_lambdas, logger):
-        # Wait until all lambdas finished execution
-        while True:
-            results = s3_client.list_objects_v2(Bucket=processing_bucket)
-            logger.info(f'Lambdas finished: {results["KeyCount"]}')
-            if results['KeyCount'] == num_of_lambdas:
-                break
+    def encode_object(object_to_encode) -> str:
+        return str(base64.b64encode(pickle.dumps(object_to_encode)))
+
+    @staticmethod
+    def get_from_s3(filename, region, bucket_name, directory):
+        s3_client = boto3.client('s3', region_name=region)
+        local_filename = os.path.join(directory, filename['Key'])
+        s3_client.download_file(bucket_name, filename['Key'], local_filename)
+
+        tfile = ROOT.TFile(local_filename, 'OPEN')
+        result = []
+        # Get all objects from TFile
+        for key in tfile.GetListOfKeys():
+            result.append(key.ReadObj())
+            result[-1].SetDirectory(0)
+        tfile.Close()
+
+        # Remove temporary root file
+        Path(local_filename).unlink()
+
+        return result
+
+    @staticmethod
+    def get_all_objects_from_s3_bucket(s3_client, bucket_name):
+        response = s3_client.list_objects_v2(Bucket=bucket_name)
+        result = response.get('Contents', [])
+        while response.get('IsTruncated'):
+            cont_token = response.get('NextContinuationToken')
+            response = s3_client.list_objects_v2(Bucket=bucket_name, ContinuationToken=cont_token)
+            result += response.get('Contents', [])
+        return result
+
+    @staticmethod
+    def invoke_root_lambda(root_range, script, region, logger):
+        """
+        Invoke root lambda.
+        Args:
+            root_range (Range): Range of data.
+            script (function): A function that performs an operation on
+                a range of data.
+            region (str): String containing AWS region.
+            logger (logging.Logger):
+        Returns:
+            bool: True if lambda invocation and execution was
+                successful.
+        """
+
+        trials = 3
+        client = boto3.client('lambda', region_name=region)
+
+        payload = json.dumps({
+            'range': AWS.encode_object(root_range),
+            'script': script,
+            'start': str(root_range.start),
+            'end': str(root_range.end),
+            'filelist': str(root_range.filelist),
+            'friend_info': AWS.encode_object(root_range.friend_info)
+        })
+
+        # Maybe here give info about number of invoked lambda for awsmonitor
+
+        while trials >= 0:
+            trials -= 1
+            try:
+                response = client.invoke(
+                    FunctionName='root_lambda',
+                    InvocationType='RequestResponse',
+                    Payload=bytes(payload, encoding='utf8')
+                )
+
+                try:
+                    response['Payload'] = json.loads(response['Payload'].read())
+                except Exception:
+                    response['Payload'] = {}
+
+                if 'FunctionError' in response or response['Payload'].get('statusCode') == 500:
+                    try:
+                        # Get error specification and remove additional quotas (side effect of serialization)
+                        error_type = response['Payload']['errorType'][1:-1]
+                        error_message = response['Payload']['errorMessage'][1:-1]
+                        exception = getattr(sys.modules['builtins'], error_type)
+                        msg = f"Lambda raised an exception: {error_message}"
+                    except Exception:
+                        exception = RuntimeError
+                        msg = (f"Lambda raised an exception: (type={response['Payload']['errorType']},"
+                               f"message={response['Payload']['errorMessage']})")
+                    raise exception(msg)
+
+            except botocore.exceptions.ClientError as error:
+                # AWS site errors
+                logger.warning(error['Error']['Message'])
+            except Exception as error:
+                # All other errors
+                logger.warning(str(error) + " (" + type(error).__name__ + ")")
+            else:
+                return True
             time.sleep(1)
 
+        # Note: lambda finishes before s3 object is created
+        return False
+
+    @staticmethod
+    def remove_all_tmp_root_files(directory):
+        for file in os.listdir(directory):
+            if file.endswith('.root'):
+                Path(os.path.join(directory, file)).unlink()
+
+    @staticmethod
+    def wait_for_all_lambdas(s3_client, processing_bucket, num_of_lambdas, logger):
+        # Wait until all lambdas finished execution
+        global lambda_await_thread_stop
+        while not lambda_await_thread_stop:
+            results = AWS.get_all_objects_from_s3_bucket(s3_client, processing_bucket)
+            logger.info(f'Lambdas finished: {len(results)}')
+            if len(results) == num_of_lambdas:
+                break
+            time.sleep(1)
 
